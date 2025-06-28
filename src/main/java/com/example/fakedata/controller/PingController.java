7ffd50bc141e3f32885ffd431eb79620c06528bf
Duong Aity <duongaity@gmail.com>
2025-06-28 15:13:04 +0700

    add swagger-ui

--- src/main/java/com/example/fakedata/controller/PingController.java
+++ src/main/java/com/example/fakedata/controller/PingController.java
@@ -2,9 +2,11 @@ package com.example.fakedata.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(value = "/api/ping")
+@RestController
+@RequestMapping(value = "/api/ping")
 public class PingController {
 
     @GetMapping
